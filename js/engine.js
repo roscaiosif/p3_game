724a60c576b3152772ea368c6245c70f2e18180c
Daniel R <roscaiosif@gmail.com>
2015-03-31 12:20:18 -0400

    Butified app.js and engine.js

--- js/engine.js
+++ js/engine.js
@@ -1,19 +1,3 @@
-/* Engine.js
- * This file provides the game loop functionality (update entities and render),
- * draws the initial game board on the screen, and then calls the update and
- * render methods on your player and enemy objects (defined in your app.js).
- *
- * A game engine works by drawing the entire game screen over and over, kind of
- * like a flipbook you may have created as a kid. When your player moves across
- * the screen, it may look like just that image/character is moving or being
- * drawn but that is not the case. What's really happening is the entire "scene"
- * is being drawn over and over, presenting the illusion of animation.
- *
- * This engine is available globally via the Engine variable and it also makes
- * the canvas' context (ctx) object globally available to make writing app.js
- * a little simpler to work with.
- */
-
 var Engine = (function(global) {
     /* Predefine the variables we'll be using within this scope,
      * create the canvas element, grab the 2D context for that canvas
@@ -25,35 +9,43 @@ var Engine = (function(global) {
         ctx = canvas.getContext('2d'),
         animID,
         lastTime,
-        counter =0;
+        counter = 0;
 
     canvas.width = 505;
     canvas.height = 606;
     doc.body.appendChild(canvas);
 
     var gameOverStyle = {
-       'font' : 'bold 46pt Georgia',
-       'fillStyle' : 'red',
-        'strokeStyle' : 'orange',
-        'textAlign': 'center'
+            'font': 'bold 46pt Georgia',
+            'fillStyle': 'red',
+            'strokeStyle': 'orange',
+            'textAlign': 'center'
         },
         scoreStyle = {
-       'font' : 'bold 24pt Georgia',
-       'fillStyle' : 'red',
-        'strokeStyle' : 'yellow',
-        'textAlign': 'left'
+            'font': 'bold 24pt Georgia',
+            'fillStyle': 'red',
+            'strokeStyle': 'yellow',
+            'textAlign': 'left'
         },
         livesStyle = {
-       'font' : 'bold 30pt Georgia',
-       'fillStyle' : 'yellow',
-        'strokeStyle' : 'orange',
-        'textAlign': 'center'
+            'font': 'bold 30pt Georgia',
+            'fillStyle': 'yellow',
+            'strokeStyle': 'orange',
+            'textAlign': 'center'
         };
 
-    var gameOver = new Text({x:canvas.width/2,y:canvas.height/2},'Game Over!'),
-        livesTxt = new Text({x:455,y:90},''),
-        scoreTxt = new Text({x:10,y:80},'');
-
+    var gameOver = new Text({
+            x: canvas.width / 2,
+            y: canvas.height / 2
+        }, 'Game Over!'),
+        livesTxt = new Text({
+            x: 455,
+            y: 90
+        }, ''),
+        scoreTxt = new Text({
+            x: 10,
+            y: 80
+        }, '');
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
@@ -70,8 +62,8 @@ var Engine = (function(global) {
         /* Call our update/render functions, pass along the time delta to
          * our update function since it may be used for smooth animation.
          */
-        if (player.lives == 0){
-            startGame=false;
+        if (player.lives == 0) {
+            startGame = false;
             player.lives = 5;
             selectSubItem = false;
             cancelAnimationFrame(animID);
@@ -81,7 +73,7 @@ var Engine = (function(global) {
             render();
             /* Set our lastTime variable which is used to determine the time delta
              * for the next time this function is called.
-            */
+             */
             lastTime = now;
             /* Use the browser's requestAnimationFrame function to call this
              * function again as soon as the browser is able to draw another frame.
@@ -98,10 +90,10 @@ var Engine = (function(global) {
         lastTime = Date.now();
         if (startGame) { //running main after selecting Play from the menu
             main();
-        }else { //render the menu and start the navigation
+        } else { //render the menu and start the navigation
             render();
-            menu.render(mdx,mdy,mStyle);
-            menu.nav('down',mdx,mdy,mStyle,xS,dyS,dySS);
+            menu.render(mdx, mdy, mStyle);
+            menu.nav('down', mdx, mdy, mStyle, xS, dyS, dySS);
         };
     }
 
@@ -115,18 +107,18 @@ var Engine = (function(global) {
      * on the entities themselves within your app.js file).
      */
     function update(dt) {
-        updateEntities(dt);
-        if (!pWin && !pDie){ //check for collision only if the player is not winning or dyeing
-            checkCollisions();
-        };
-    }
-    /* This is called by the update function  and loops through all of the
-     * objects within your allEnemies array as defined in app.js and calls
-     * their update() methods. It will then call the update function for your
-     * player object. These update methods should focus purely on updating
-     * the data/properties related to  the object. Do your drawing in your
-     * render methods.
-     */
+            updateEntities(dt);
+            if (!pWin && !pDie) { //check for collision only if the player is not winning or dyeing
+                checkCollisions();
+            };
+        }
+        /* This is called by the update function  and loops through all of the
+         * objects within your allEnemies array as defined in app.js and calls
+         * their update() methods. It will then call the update function for your
+         * player object. These update methods should focus purely on updating
+         * the data/properties related to  the object. Do your drawing in your
+         * render methods.
+         */
     function updateEntities(dt) {
         allEnemies.forEach(function(Enemy) {
             Enemy.update(dt);
@@ -134,56 +126,61 @@ var Engine = (function(global) {
         allTreasures.forEach(function(Treasure) {
             Treasure.update(dt);
         });
-        var lt =allTreasures.length,n; //add treasures after 500 cycles or when they were collected
-        if ( lt== 0){
+        var lt = allTreasures.length,
+            n; //add treasures after 500 cycles or when they were collected
+        if (lt == 0) {
             counter++;
-         };
-         if (counter>500 && lt < 3) {
-            n = Math.round(Math.random()*2);
+        };
+        if (counter > 500 && lt < 3) {
+            n = Math.round(Math.random() * 2);
             addTreasures(n);
             counter = 0;
-         };
+        };
         player.update(dt);
     }
 
-    function detectCollision(entity,n){
+    function detectCollision(entity, n) {
         /*detects the collision if the CM of the two entities
         are less then a fraction of their frame-dimensions*/
-        var dx,dy;
-        for(var i=0;i<n;i++){
-            dx =entity[i].centerX-player.centerX;
-            if (dx<0){dx = -dx};
-            dy =entity[i].centerY-player.centerY;
-            if (dy<0){dy = -dy};
-            if((dx<=entity[i].width/3 + player.width/3)&&
-               (dy<=entity[i].height/3 + player.height/3)){
+        var dx, dy;
+        for (var i = 0; i < n; i++) {
+            dx = entity[i].centerX - player.centerX;
+            if (dx < 0) {
+                dx = -dx
+            };
+            dy = entity[i].centerY - player.centerY;
+            if (dy < 0) {
+                dy = -dy
+            };
+            if ((dx <= entity[i].width / 3 + player.width / 3) &&
+                (dy <= entity[i].height / 3 + player.height / 3)) {
                 return [true, i];
             };
         };
         return [false, i];
     }
 
-    function checkCollisions(){
+    function checkCollisions() {
         var n = allEnemies.length,
-            nt =allTreasures.length,
+            nt = allTreasures.length,
             result;
-        result = detectCollision(allEnemies,n);
-        if (result[0]){
+        result = detectCollision(allEnemies, n);
+        if (result[0]) {
             //colliding with an enemy
-            pDie =true;
+            pDie = true;
         };
-        result = detectCollision(allTreasures,nt);
-        if (result[0]){
+        result = detectCollision(allTreasures, nt);
+        if (result[0]) {
             //collecting treasure items they have to disappear
-            switch (result[1]){
+            switch (result[1]) {
                 case 0:
                     allTreasures.shift();
-                break;
+                    break;
                 case 1:
                     allTreasures.pop();
-                break;
+                    break;
                 default:
-                    allTreasures.splice(result[1],1);
+                    allTreasures.splice(result[1], 1);
             };
             player.score += 10;
             counter = 0;
@@ -201,12 +198,12 @@ var Engine = (function(global) {
          * for that particular row of the game level.
          */
         var rowImages = [
-                'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+                'images/water-block.png', // Top row is water
+                'images/stone-block.png', // Row 1 of 3 of stone
+                'images/stone-block.png', // Row 2 of 3 of stone
+                'images/stone-block.png', // Row 3 of 3 of stone
+                'images/grass-block.png', // Row 1 of 2 of grass
+                'images/grass-block.png' // Row 2 of 2 of grass
             ],
             numRows = 6,
             numCols = 5,
@@ -249,15 +246,15 @@ var Engine = (function(global) {
         });
 
         player.render();
-        if (pWin){ //attaching winning text to the player
+        if (pWin) { //attaching winning text to the player
             winText.x = player.centerX;
-            winText.y = player.centerY-40;
-            winText.render(0,0,wStyle);
+            winText.y = player.centerY - 40;
+            winText.render(0, 0, wStyle);
         };
-        if (pDie){ //attaching dying text to the player
+        if (pDie) { //attaching dying text to the player
             dieText.x = player.centerX;
-            dieText.y = player.centerY-40;
-            dieText.render(0,0,dieStyle);
+            dieText.y = player.centerY - 40;
+            dieText.render(0, 0, dieStyle);
         };
     }
 
@@ -266,25 +263,25 @@ var Engine = (function(global) {
      * those sorts of things. It's only called once by the init() method.
      */
     function reset() {
-       //this was implemented in different other functions
+        //this was implemented in different other functions
     }
 
-    function renderText(){
+    function renderText() {
         //Game over
-        if (player.lives==0){
-            player.score =0;
-            gameOver.render(0,0,gameOverStyle);
+        if (player.lives == 0) {
+            player.score = 0;
+            gameOver.render(0, 0, gameOverStyle);
         }
         //lives
-        ctx.drawImage(Resources.get('images/Heart.png'), 4*101,0);
+        ctx.drawImage(Resources.get('images/Heart.png'), 4 * 101, 0);
         livesTxt.str = player.lives.toString();
-        livesTxt.render(0,0,livesStyle);
+        livesTxt.render(0, 0, livesStyle);
         //score
-        scoreTxt.str = 'Score '+ player.score.toString();
-        scoreTxt.render(0,0,scoreStyle);
+        scoreTxt.str = 'Score ' + player.score.toString();
+        scoreTxt.render(0, 0, scoreStyle);
         //help
-        helpTxt.render(0,0,helpStyle);
-   }
+        helpTxt.render(0, 0, helpStyle);
+    }
 
     /* Go ahead and load all of the images we know we're going to need to
      * draw our game level. Then set init as the callback method, so that when
